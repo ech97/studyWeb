f9bd6f4503c578099e7a4b484b280541ca3efe4a
chan <ech97@naver.com>
2021-01-16 21:26:04 +0900

    web

---
+++ dreamcoding/4_JS/variable.js
@@ -0,0 +1,29 @@
+// 개발공부를 위해 꼼꼼하게 체크하라는 선언
+'use strict';
+
+
+
+
+function init(){
+
+}
+
+let name = 'ellie';
+console.log(name);
+name = 'hello';
+console.log(name);
+
+
+
+init();
+
+
+/*
+function, first-class function // JS에서는 함수도 변수로 할당됨. (class 할당되는거마냥) // = first class func
+
+number = int, float 등등을 number로 통합
+프로그램이 동작하면서 알아서 형 변환함
+*/
+
+// object
+const ellie = { name: 'Chan', age: 20};
