12966f92b34b01e52f1da456255fcfe613eab282
chan <ech97@naver.com>
2021-01-20 22:35:28 +0900

    21/01/20 22:35

--- dreamcoding/4_JS/variable.js
+++ dreamcoding/4_JS/variable.js
@@ -42,3 +42,61 @@ function printAll(...args){
 
 
 printAll('dream', 'coding', 'ellie');
+
+
+
+// 함수의 호출명을 설정하지않은게 그냥 Anonymous Function 이었따니ㅋㅋㅋ 이름붙이면 named func.
+// 함수도 hoisting 돼서, 선언 전에 호출가능. 
+const joint = function() {
+    console.log('print');
+};
+
+joint(); // 이렇게 함수 호출 가능
+
+
+// Arrow Function은 결국 이걸 간단하게 만들어주는아이
+const add = (a, b) => a + b;
+
+const add1 = function(a, b) {
+    return a + b;
+};
+
+const add2 = (a, b) =>  {
+    // do something more
+    return a*b;
+};
+
+
+// IIFE : 함수를 바로 실행
+(function hello() {
+    console.log('IIFE');
+})();
+
+
+
+const Calculate = (cmd, a, b) => {
+    switch (cmd) {
+        case 'add':
+            return a+b;
+            break;
+        case 'sub':
+            return a-b;
+            break;
+        case 'mul':
+            return a*b;
+            break;
+        case 'div':
+            return a/b;
+            break;
+
+        default:
+            console.log(`${cmd}는 없는 명령어입니다.`)
+    }
+}
+
+const result = Calculate('add', 1, 3);
+
+
+
+
+// class = fields + methods
