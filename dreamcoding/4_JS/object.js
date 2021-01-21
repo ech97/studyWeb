9b6e3cbd64092c57477492d407be43a3a893d40e
chan <ech97@naver.com>
2021-01-21 15:11:02 +0900

    21.01.21 15:10

---
+++ dreamcoding/4_JS/object.js
@@ -0,0 +1,113 @@
+'use strict'
+
+// 오브젝트는 그냥 자주쓰이는거 틀로 만들어놓으면 되는거니깐
+
+const obj1 = {}; // obj literal, 구조체와 비슷하네
+const obj2 = new fishBread('chanHyunLee', 25); // obj constructor
+
+const obj3 = {
+    _name: 'chanhyun',
+    _age: 25,
+
+    get name() {
+        console.log('get 호출됨');
+        return this._name;
+    },
+
+    set name(value) {
+        console.log('set 호출됨');
+        this._name= value;
+    }
+};
+
+obj3.name;
+obj3.name = "이찬현";
+obj3._age;
+delete obj3._age;
+// 나중에 Object의 요소를 삭제하거나 추가할수있음. (미침)
+
+
+
+// 2. Computed properties
+console.log(obj2.age);
+console.log(obj2['name']);
+obj2['hasJob'] = true; // 이렇게 요소 추가 가능
+
+
+
+// 2-1. Computed properties는 다음과같이 활용가능
+function printValue(obj, key){
+    console.log(obj[key])
+}
+
+printValue(obj2, 'name')
+
+
+
+// 3. Property value shorthand
+const Person1 = {name: 'bob', age: 2};
+const Person2 = {name: 'dave', age: 24};
+const Person3 = {name: 'steve', age: 40};
+
+const Person4 = makePerson('ellie', 30);
+
+//이렇게 구조체를 리턴하는 함수를 작성해서 사용가능
+function makePerson(name, age){
+    return {
+    name: name,
+    age: age
+    };
+}
+
+function makePerson2(name, age){
+    return {
+        name,
+        age
+    };
+} // 다음과같이 입력받은 인자그대로 key에 사용하는경우 생략 가능
+
+// 이런식으로 IIFE를 활용한 방식도 사용가능
+const Person5 = (function makePerson3(name, age){
+    return {
+        name,
+        age
+    };
+})('chanHyun', 25);
+
+
+
+// 4. 어느정도의 규칙이 있음
+const fish1 = {name: '붕어', size: 7};
+const fish2 = {name: '가재', size: 10};
+const fish3 = new Fish('ellie', 100);
+
+// 이렇게 구조체를 리턴해주는 애들은 앞에 대문자를 씀
+function Fish(name, size){
+    // this = {};
+    this.name = name;
+    this.size = size;
+    // return this;
+}
+
+
+// 5. in op
+console.log('name' in Fish);
+
+
+// 6. for..in vs for..of
+// for (key in obj)
+console.clear();
+for (const key in fish3){
+    console.log(key);
+}
+
+const array = [1, 2, 3, 4, 5];
+for(let i = 0; i < array.length; i++){
+    console.log(i);
+}
+for(const value of array){
+    console.log(value);
+}
+
+
+console.clear();
--- dreamcoding/4_JS/variable.js
+++ dreamcoding/4_JS/variable.js
@@ -100,3 +100,93 @@ const result = Calculate('add', 1, 3);
 
 
 // class = fields + methods
+// Class는 틀, 그걸 채운게 Object
+class fishBread {
+    // 생성자임 constructor
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    // public, private로 변수 선언방법은 #을 붙이냐 안붙이냐
+    
+    // static을 사용하면, '클래스'고유의 변수가 됨. 인스턴스한 애들은 못봄
+    // 진짜 ㅈㄴ 자주쓰이는 변수가 있따면, 굳이 인스턴스까지 안해도되게. 그냥 클래스에서 바로 부를수있도록.
+
+
+    // Method
+    speak() {
+        console.log(`${this.name}: Hello!`);
+    }
+
+    //getter, setter .. private한 멤버변수를 수정하거나 가져오기위한 함수를 별도 제작해야함.
+    //여기선 name과 age가 private한 멤버변수임.
+    //따라서 생성자도 멤버변수 초기화를 위해선 get함수와 set함수를 사용해야함
+    get age() {
+        console.log('값을 호출했습니다.');
+        return this._age;
+    }
+
+    set age(value){
+        // this.age = value; // 이렇게 선언하게 된다면, 
+        // private 멤버변수 this.age를 value로 초기화하기위해 set()을 계속 호출함
+        console.log('값을 대입했습니다');
+        this._age = value;
+
+        this._age = value < 0 ? 0 : value;
+    }
+
+    // 전체적인 Sequence
+    // this.age = value 을 실행하게되면
+    // setter함수가 실행되어 _age라는 변수에 value 값 저장.
+    // 이후에 this.age를 호출할때,
+    // getter함수가 실행되어 _age가 호출됨
+}
+
+
+
+class Shape {
+    constructor(width, height, color){
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw(){
+        console.log(`drawing ${this.color} color of`);
+    }
+
+    getArea(){
+        return width * this.height;
+    }
+}
+
+// 상속
+class Rectangle extends Shape {}
+class Triangel extends Shape {
+    draw() {
+        super.draw(); // 부모의 메소드를 호출! // 완전 덮어쓰기 되지 않게!
+        console.log('😚')
+    }
+    getArea(){
+        return (this.width * this.height) / 2;
+    }
+}
+const rectangle = new Rectangle(20, 20, 'blue');
+
+rectangle.draw();
+
+
+// instanceof. 특정 Object가 특정 Class의 인스턴스인지 확인하는 명령
+console.log(rectangle instanceof Rectangle);
+
+// 자바스크립트의 모든 Object는 다 Object를 상속받아 제작됨.
+console.log(rectangle instanceof Object);
+
+
+
+
+
+
+
+
